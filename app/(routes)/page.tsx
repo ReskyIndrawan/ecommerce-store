e0b0d654deabc90903c2ef9a0a6a0addca7c6fbe
Resky Indrawan <reskyindrawan@ReskynoMacBook-Air.local>
2023-09-12 04:33:56 +0900

    final

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard(
+    "8c3e9fc5-9f82-4d8b-8b6a-c9da03c6393f"
+  );
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
